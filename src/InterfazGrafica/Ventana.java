3b8fc148833a8659b27a76ddd2fdefdebecd8881
any67 <any_sf_1967@hotmail.com>
2017-11-08 20:49:38 -0300

    importamos libreria

---
+++ src/InterfazGrafica/Ventana.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InterfazGrafica;
+//PASO 1 - IMPORTAMOS LAS LIBRERIAS
+import javax.swing.*;//iMPORTAMOS TODO DE SWING
+import java.awt.*;
+
+/**
+ *
+ * @author LAB08
+ */
+//PASO 2 -HEREDAR LA CLASE DESDE JFRAME
+public class  Ventana extends JFrame
+        
+{
+  //PASO 3 -DECLARAR ELEMENTOS A UTILIZAR
+    JPanel panel;//Creacion de objeto JPanel
+    JLabel label;
+    JTextField text;
+    JButton boton;  
+}
